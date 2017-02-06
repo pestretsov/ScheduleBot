5773beedd1c94f1d971d4aac62f88bee015cb6ca
Roman Golyshev <anstreth@gmail.com>
2017-02-06 08:32:38 +0300

    Inital heroku commit

---
+++ src/main/java/org/anstreth/SchedulerBotApplication.java
@@ -0,0 +1,12 @@
+package org.anstreth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerBotApplication.class, args);
+	}
+}

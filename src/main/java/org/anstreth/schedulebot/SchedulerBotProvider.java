8a09c0295c48f001fe5ee68f63ce278966a8e844
pestretsov <pestretsov@me.com>
2017-02-25 18:47:52 +0300

    remove bot token from application.properties

--- src/main/java/org/anstreth/schedulebot/SchedulerBotProvider.java
+++ src/main/java/org/anstreth/schedulebot/SchedulerBotProvider.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @DependsOn("telegramBotsApi")
+@PropertySource("classpath:bot.properties")
 class SchedulerBotProvider {
 
     @Value("${bot.token}")

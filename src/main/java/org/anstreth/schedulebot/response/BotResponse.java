f585091645e51cd5b1298ed33a8ea9195d2a10c3
Roman Golyshev <anstreth@gmail.com>
2017-07-06 17:58:57 +0300

    #18-remove-MessageSender: add convenient constructors to `BotResponse`

--- src/main/java/org/anstreth/schedulebot/response/BotResponse.java
+++ src/main/java/org/anstreth/schedulebot/response/BotResponse.java
@@ -1,11 +1,22 @@
 package org.anstreth.schedulebot.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Collections;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class BotResponse {
     private final List<String> messages;
     private final List<String> replies;
+
+    public BotResponse(List<String> messages) {
+        this(messages, Collections.emptyList());
+    }
+
+    public BotResponse(String messages) {
+        this(Collections.singletonList(messages));
+    }
 }

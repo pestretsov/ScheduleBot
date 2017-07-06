850cf26ed9e48dc59b5fb5100d67bddbeafeb3a2
Roman Golyshev <anstreth@gmail.com>
2017-07-06 18:28:06 +0300

    #18-remove-MessageSender: add method to `UserGroupSearchService` to return `BotResponse`

--- src/main/java/org/anstreth/schedulebot/schedulebotservice/UserGroupSearchService.java
+++ src/main/java/org/anstreth/schedulebot/schedulebotservice/UserGroupSearchService.java
@@ -2,6 +2,7 @@ package org.anstreth.schedulebot.schedulebotservice;
 
 import org.anstreth.ruzapi.response.Group;
 import org.anstreth.schedulebot.exceptions.NoSuchGroupFoundException;
+import org.anstreth.schedulebot.response.BotResponse;
 import org.anstreth.schedulebot.schedulebotservice.request.UserRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -27,6 +28,16 @@ public class UserGroupSearchService {
         }
     }
 
+    BotResponse tryToFindUserGroup(UserRequest userRequest, List<String> successReplies) {
+        try {
+            Group userGroup = findUserGroup(userRequest);
+            return getResponseAboutProperGroupSetting(userGroup, successReplies);
+        } catch (NoSuchGroupFoundException e) {
+            return sendMessageAboutIncorrectGroupName(userRequest);
+        }
+    }
+
+
     private Group findUserGroup(UserRequest userRequest) {
         return userGroupManager.findAndSetGroupForUser(userRequest.getUserId(), userRequest.getMessage());
     }
@@ -36,9 +47,20 @@ public class UserGroupSearchService {
         messageSender.sendMessage(message);
     }
 
+    private BotResponse getResponseAboutProperGroupSetting(Group group, List<String> replies) {
+        String message = String.format("Your group is set to '%s'.", group.getName());
+        return new BotResponse(message, replies);
+    }
+
     private void sendMessageAboutIncorrectGroupName(MessageSender messageSender, UserRequest userRequest) {
         String message = String.format("No group by name '%s' is found! Try again.", userRequest.getMessage());
         messageSender.sendMessage(message);
     }
+
+    private BotResponse sendMessageAboutIncorrectGroupName(UserRequest userRequest) {
+        String message = String.format("No group by name '%s' is found! Try again.", userRequest.getMessage());
+        return new BotResponse(message);
+    }
+
 
 }

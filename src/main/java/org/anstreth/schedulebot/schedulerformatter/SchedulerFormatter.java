c3414cbff47a4086530cd54bb35a366f30a3ba93
Roman Golyshev <anstreth@gmail.com>
2017-07-07 23:58:08 +0300

    #18-remove-MessageSender: add note about visitor pattern to `SchedulerFormatter`

--- src/main/java/org/anstreth/schedulebot/schedulerformatter/SchedulerFormatter.java
+++ src/main/java/org/anstreth/schedulebot/schedulerformatter/SchedulerFormatter.java
@@ -4,15 +4,18 @@ import org.anstreth.schedulebot.schedulerbotcommandshandler.response.*;
 
 import java.util.List;
 
+/**
+ * This interface is supposed to be used as a visitor.
+ *
+ * All of its methods return Lists because some responses may have many messages inside.
+ * Particular one is a WeekResponse.
+ *
+ * @author Roman Golyshev
+ */
 public interface SchedulerFormatter {
-
-    List<String> format(NoScheduleForDayResponse response);
-
-    List<String> format(NoScheduleForWeekResponse response);
-
     List<String> format(DayResponse dayResponse);
-
     List<String> format(WeekResponse weekResponse);
-
     List<String> format(SimpleStringResponse response);
+    List<String> format(NoScheduleForDayResponse response);
+    List<String> format(NoScheduleForWeekResponse response);
 }

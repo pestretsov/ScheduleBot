4303d00e647aa83aa357e8ce204dc191071d8e80
Roman Golyshev <anstreth@gmail.com>
2017-07-07 19:48:24 +0300

    #21-scheduler-formatter-visitor: add `DayFormatter` to `ScheduleFormatter`

--- src/main/java/org/anstreth/schedulebot/schedulerformatter/SimpleSchedulerFormatter.java
+++ src/main/java/org/anstreth/schedulebot/schedulerformatter/SimpleSchedulerFormatter.java
@@ -1,24 +1,18 @@
 package org.anstreth.schedulebot.schedulerformatter;
 
+import java.util.Calendar;
 import org.anstreth.ruzapi.response.Day;
-import org.anstreth.ruzapi.response.Lesson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 class SimpleSchedulerFormatter implements SchedulerFormatter {
 
-    private final LessonFormatter lessonFormatter;
-    private final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE, yyyy-MM-dd");
+    private final DayFormatter dateFormatter;
 
     @Autowired
-    public SimpleSchedulerFormatter(LessonFormatter lessonFormatter) {
-        this.lessonFormatter = lessonFormatter;
+    public SimpleSchedulerFormatter(LessonFormatter lessonFormatter, DayFormatter dateFormatter) {
+        this.dateFormatter = dateFormatter;
     }
 
     @Override
@@ -33,22 +27,7 @@ class SimpleSchedulerFormatter implements SchedulerFormatter {
 
     @Override
     public String formatDay(Day scheduleForToday) {
-        String lessons = getLessonsString(scheduleForToday.getLessons());
-        return String.format("Schedule for %s:\n\n%s", formatDate(scheduleForToday), lessons);
-    }
-
-    private String formatDate(Day scheduleForToday) {
-        return simpleDateFormat.format(scheduleForToday.getDate());
-    }
-
-    private String getLessonsString(List<Lesson> lessons) {
-        if (lessons.isEmpty()) {
-            return "There are no lessons for this day!";
-        }
-
-        return lessons.stream()
-                .map(lessonFormatter::formatLesson)
-                .collect(Collectors.joining("\n\n"));
+        return dateFormatter.formatDay(scheduleForToday);
     }
 
 }
--- src/test/java/org/anstreth/schedulebot/schedulerformatter/SimpleSchedulerFormatterTest.java
+++ src/test/java/org/anstreth/schedulebot/schedulerformatter/SimpleSchedulerFormatterTest.java
@@ -1,21 +1,17 @@
 package org.anstreth.schedulebot.schedulerformatter;
 
 import org.anstreth.ruzapi.response.Day;
-import org.anstreth.ruzapi.response.Lesson;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Collections;
-import java.util.Date;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
-import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -24,46 +20,15 @@ public class SimpleSchedulerFormatterTest {
     private SimpleSchedulerFormatter schedulerFormatter;
 
     @Mock
-    private LessonFormatter lessonFormatter;
+    private DayFormatter dayFormatter;
 
     @Test
-    public void formatterProducesStringStartingFromCertainWordsAndNameOfDayAndDate() throws Exception {
-        Date date = new Date();
-        Day scheduleForToday = new Day();
-        scheduleForToday.setLessons(Collections.emptyList());
-        String expectedDayFormat = getExpectedDayFormat(date);
-        scheduleForToday.setDate(date);
-        String expectedPrefix = String.format("Schedule for %s:\n\n", expectedDayFormat);
+    public void dayFormattedWith_dayFormatter() throws Exception {
+        Day mockDay = mock(Day.class);
+        String formattedDay = "formattedDay";
+        doReturn(formattedDay).when(dayFormatter).formatDay(mockDay);
 
-        String formattedResult = schedulerFormatter.formatDay(scheduleForToday);
-
-        assertThat(formattedResult, startsWith(expectedPrefix));
-    }
-
-    @Test
-    public void everyLessonIsFromattedWithLessonFromatter() throws Exception {
-        Lesson lesson = mock(Lesson.class);
-        Day dayWithLessons = new Day();
-        dayWithLessons.setDate(new Date());
-        dayWithLessons.setLessons(Collections.singletonList(lesson));
-        String formattedLessonString = "formatted lesson string";
-        given(lessonFormatter.formatLesson(lesson)).willReturn(formattedLessonString);
-
-        String formattedResult = schedulerFormatter.formatDay(dayWithLessons);
-
-        assertThat(formattedResult, endsWith(formattedLessonString));
-    }
-
-    @Test
-    public void ifThereAreNoLessonsThereAreExpectedPlaceholder() throws Exception {
-        Day dayWithoutLessons = new Day();
-        dayWithoutLessons.setDate(new Date());
-        dayWithoutLessons.setLessons(Collections.emptyList());
-        String expectedPlaceholder = "There are no lessons for this day!";
-
-        String formattedReslut = schedulerFormatter.formatDay(dayWithoutLessons);
-
-        assertThat(formattedReslut, endsWith(expectedPlaceholder));
+        assertThat(schedulerFormatter.formatDay(mockDay), is(formattedDay));
     }
 
     @Test
@@ -78,10 +43,6 @@ public class SimpleSchedulerFormatterTest {
         String message = schedulerFormatter.getNoScheduleForWeekMessage(Calendar.getInstance());
 
         assertThat(message, is("There are no schedule for this week!"));
-    }
-
-    private String getExpectedDayFormat(Date date) {
-        return new SimpleDateFormat("EEEE, yyyy-MM-dd").format(date);
     }
 
 }

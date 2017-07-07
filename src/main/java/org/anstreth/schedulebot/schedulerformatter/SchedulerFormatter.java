30c02838ce41d497b36cfbb24fd5ffc20b9623b8
Roman Golyshev <anstreth@gmail.com>
2017-07-07 19:50:38 +0300

    #21-scheduler-formatter-visitor: add default specific methods to `SchedulerFormatter` interface

--- src/main/java/org/anstreth/schedulebot/schedulerformatter/SchedulerFormatter.java
+++ src/main/java/org/anstreth/schedulebot/schedulerformatter/SchedulerFormatter.java
@@ -1,6 +1,14 @@
 package org.anstreth.schedulebot.schedulerformatter;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.anstreth.ruzapi.response.Day;
+import org.anstreth.schedulebot.schedulerbotcommandshandler.response.DayResponse;
+import org.anstreth.schedulebot.schedulerbotcommandshandler.response.NoScheduleForDayResponse;
+import org.anstreth.schedulebot.schedulerbotcommandshandler.response.NoScheduleForWeekResponse;
+import org.anstreth.schedulebot.schedulerbotcommandshandler.response.SimpleStringResponse;
+import org.anstreth.schedulebot.schedulerbotcommandshandler.response.WeekResponse;
 import org.springframework.stereotype.Component;
 
 import java.util.Calendar;
@@ -10,4 +18,26 @@ public interface SchedulerFormatter {
     String formatDay(Day scheduleForToday);
     String getNoScheduleForDateMessage(Calendar calendar);
     String getNoScheduleForWeekMessage(Calendar calendar);
+
+    default List<String> format(DayResponse dayResponse) {
+        return Collections.singletonList(formatDay(dayResponse.getDay()));
+    }
+
+    default List<String> format(WeekResponse weekResponse) {
+        return weekResponse.getWeekSchedule().getDays().stream()
+            .map(this::formatDay)
+            .collect(Collectors.toList());
+    }
+
+    default List<String> format(NoScheduleForDayResponse response) {
+        return Collections.singletonList(getNoScheduleForDateMessage(response.getDate()));
+    }
+
+    default List<String> format(NoScheduleForWeekResponse response) {
+        return Collections.singletonList(getNoScheduleForWeekMessage(response.getDate()));
+    }
+
+    default List<String> format(SimpleStringResponse response) {
+        return Collections.singletonList(response.getMessage());
+    }
 }

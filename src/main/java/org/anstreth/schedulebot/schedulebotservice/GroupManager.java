21fe33bec441bae7449625403ff7b8cf8117b933
Roman Golyshev <anstreth@gmail.com>
2017-07-12 01:49:28 +0300

    #24-removing-exceptions-from-bot-service: add empty `GroupManager` class

---
+++ src/main/java/org/anstreth/schedulebot/schedulebotservice/GroupManager.java
@@ -0,0 +1,11 @@
+package org.anstreth.schedulebot.schedulebotservice;
+
+import org.anstreth.ruzapi.response.Group;
+import org.springframework.stereotype.Component;
+
+@Component
+class GroupManager {
+    Group findGroupByName(String groupName) {
+        return null;
+    }
+}

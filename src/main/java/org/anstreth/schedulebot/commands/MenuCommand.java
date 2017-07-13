164c5beacbb20356a0c24b19c69eee37acfe0d22
Roman Golyshev <anstreth@gmail.com>
2017-07-13 19:23:39 +0300

    #14-change-group-command: add `MenuCommand` enum

---
+++ src/main/java/org/anstreth/schedulebot/commands/MenuCommand.java
@@ -0,0 +1,19 @@
+package org.anstreth.schedulebot.commands;
+
+/**
+ * Enum to represent possible user commands in menu state.
+ */
+
+public enum MenuCommand {
+
+    /**
+     * Command to get back to normal state.
+     */
+    BACK,
+
+    /**
+     * Command to remove user's group and set his state to NO_GROUP
+     */
+    RESET_GROUP
+
+}

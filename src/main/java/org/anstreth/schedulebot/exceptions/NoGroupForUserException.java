7edf2ab143cf7f42e09d5ee6cd0e8f25cc708e61
Roman Golyshev <anstreth@gmail.com>
2017-08-01 21:47:16 +0300

    user-group-repository: remove unused exceptions

--- src/main/java/org/anstreth/schedulebot/exceptions/NoGroupForUserException.java
+++
@@ -1,9 +0,0 @@
-package org.anstreth.schedulebot.exceptions;
-
-public class NoGroupForUserException extends RuntimeException {
-    public NoGroupForUserException() {}
-
-    public NoGroupForUserException(long userId) {
-        super("No group is specified for user with id " + userId);
-    }
-}
--- src/main/java/org/anstreth/schedulebot/exceptions/NoSuchUserException.java
+++
@@ -1,7 +0,0 @@
-package org.anstreth.schedulebot.exceptions;
-
-public class NoSuchUserException extends RuntimeException {
-    public NoSuchUserException(long userId) {
-        super("No user with id " + userId + "is found");
-    }
-}

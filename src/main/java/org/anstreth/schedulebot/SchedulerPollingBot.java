8d0ab83c91f996f2efbacb2a3077c5eed742ccec
Roman Golyshev <anstreth@gmail.com>
2017-11-01 08:37:17 +0300

    #33-add-exception-handling-and-timeouts: add exception handling in `SchedulerPollingBot`

--- src/main/java/org/anstreth/schedulebot/SchedulerPollingBot.java
+++ src/main/java/org/anstreth/schedulebot/SchedulerPollingBot.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j;
 import org.anstreth.schedulebot.response.BotResponse;
 import org.anstreth.schedulebot.schedulebotservice.UserRequestRouter;
 import org.anstreth.schedulebot.schedulebotservice.request.UserRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboard;
@@ -13,6 +14,7 @@ import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,6 +24,9 @@ class SchedulerPollingBot extends TelegramLongPollingBot {
     private final String token;
 
     private final UserRequestRouter userRequestRouter;
+
+    @Value("${ruzapi.defaultErrorMessage}")
+    private String DEFAULT_ERROR_MESSAGE;
 
     SchedulerPollingBot(String token, UserRequestRouter userRequestRouter) {
         super();
@@ -45,8 +50,18 @@ class SchedulerPollingBot extends TelegramLongPollingBot {
             Long chatId = update.getMessage().getChatId();
             UserRequest userRequest = getUserRequestFromUpdate(update);
             userRequestRouter.routeAsync(userRequest)
-                    .thenAccept(response -> sendBotResponseToChat(chatId, response));
+                .handle((response, e) -> handleBotResponse(chatId, response, e))
+                .thenAccept(messages -> messages.forEach(this::sendMessageQuietly));
         }
+    }
+
+    private List<SendMessage> handleBotResponse(long chatId, BotResponse response, Throwable e) {
+        if (response != null) {
+            return getSendMessagesForBotResponse(chatId, response);
+        }
+
+        log.error("Error happened during handling request. Sending default error response.", e);
+        return Collections.singletonList(new SendMessage(chatId, DEFAULT_ERROR_MESSAGE));
     }
 
     private boolean updateHasMessageWithText(Update update) {
@@ -57,11 +72,6 @@ class SchedulerPollingBot extends TelegramLongPollingBot {
         Long chatId = update.getMessage().getChatId();
         String messageText = update.getMessage().getText();
         return new UserRequest(chatId, messageText);
-    }
-
-    private void sendBotResponseToChat(Long chatId, BotResponse botResponse) {
-        getSendMessagesForBotResponse(chatId, botResponse)
-                .forEach(this::sendMessageQuietly);
     }
 
     private List<SendMessage> getSendMessagesForBotResponse(Long chatId, BotResponse botResponse) {

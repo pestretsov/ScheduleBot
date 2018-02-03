1a8416a5e69bc3aac195c4db81b73b11968c241e
pestretsov <pestretsov@me.com>
2018-02-03 22:04:14 +0300

    add-group-chat-support: add lombok constructor annotation to User model

--- src/main/java/org/anstreth/schedulebot/model/User.java
+++ src/main/java/org/anstreth/schedulebot/model/User.java
@@ -1,16 +1,13 @@
 package org.anstreth.schedulebot.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class User {
     private final long id;
     private final UserState state;
-
-    public User(long id, UserState state) {
-        this.id = id;
-        this.state = state;
-    }
 
     public User(long id) {
         this(id, UserState.NO_GROUP);
